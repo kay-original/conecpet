dfcfe3e0ef659662260d8784d1df8585f1aebc65
長岡快 <nagaokakai@nagaokakainoMacBook-Air.local>
2021-07-23 00:02:32 +0900

    データベース変更

--- db/migrate/20210717110111_change_date_mother_breed_to_pets.rb
+++
@@ -1,7 +0,0 @@
-class ChangeDateMotherBreedToPets < ActiveRecord::Migration[6.1]
-  def change
-    change_column :pets, :breed, :integer
-    change_column :pets, :father_breed, :integer
-    change_column :pets, :mother_breed, :integer
-  end
-end
--- db/migrate/20210717110553_change_date_breed_to_pets.rb
+++
@@ -1,7 +0,0 @@
-class ChangeDateBreedToPets < ActiveRecord::Migration[6.1]
-  def change
-    change_column :pets, :breed, :string
-    change_column :pets, :father_breed, :string
-    change_column :pets, :mother_breed, :string
-  end
-end
